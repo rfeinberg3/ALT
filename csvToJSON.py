4fbc8b808b4b917468fdaad5e53c457620005bf4
rfeinberg3 <rfeinberg3@gmail.com>
2023-11-16 08:33:11 -0500

    csvToJSON working

--- csvToJSON.py
+++ csvToJSON.py
@@ -1,6 +1,20 @@
 import csv
 import json
 
+# Decide the two file paths according to your 
+# computer system
+csvFilePath = r'/Users/ryan/Desktop/NLPFinalProject/Hittite Sheet - w2w.csv'
+jsonFilePath = r'/Users/ryan/Desktop/NLPFinalProject/Hittite Sheet - w2w.json'
+csvfile = open(csvFilePath, 'r')
+jsonfile = open(jsonFilePath, 'w')
+
+fieldnames = ("Hittite", "translation")
+reader = csv.DictReader( csvfile, fieldnames)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+
+'''
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments
 def make_json(csvFilePath, jsonFilePath):
@@ -18,7 +32,7 @@ def make_json(csvFilePath, jsonFilePath):
              
             # Assuming a column named 'No' to
             # be the primary key
-            #key = rows['No']
+            #=key = rows['No']
             #data[key] = rows
             data = rows
  
@@ -29,10 +43,8 @@ def make_json(csvFilePath, jsonFilePath):
          
 # Driver Code
  
-# Decide the two file paths according to your 
-# computer system
-csvFilePath = r'/Users/ryan/Desktop/hittiteJSONFile/Hittite Sheet - w2w.csv'
-jsonFilePath = r'/Users/ryan/Desktop/hittiteJSONFile/Hittite Sheet - w2w.json'
+
  
 # Call the make_json function
 make_json(csvFilePath, jsonFilePath)
+'''
