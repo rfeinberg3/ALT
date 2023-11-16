77571600e6716a5f51904cafbf4dfab76ec51751
rfeinberg3 <rfeinberg3@gmail.com>
2023-11-16 09:57:00 -0500

    data scrubbed

--- csvToJSON.py
+++ csvToJSON.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 
 # Decide the two file paths according to your 
 # computer system
@@ -8,43 +9,18 @@ jsonFilePath = r'/Users/ryan/Desktop/NLPFinalProject/Hittite Sheet - w2w.json'
 csvfile = open(csvFilePath, 'r')
 jsonfile = open(jsonFilePath, 'w')
 
-fieldnames = ("Hittite", "translation")
 reader = csv.DictReader( csvfile, fieldnames)
+fieldnames = ("Hittite", "Translation")
+reader = csv.DictReader(csvfile, fieldnames)
 for row in reader:
+    # use regex to scrub "Hittie" labeled data from csv
+    pattern_text = r'\w+' 
+    pattern = re.compile(pattern_text)
+    hitScraped = pattern.match(row["Hittite"]) # match only first word from "Hittite" labeled data
+    if hitScraped != None:
+        row["Hittite"] = hitScraped.group(0)
+    else:
+        continue
+
     json.dump(row, jsonfile)
     jsonfile.write('\n')
-
-'''
-# Function to convert a CSV to JSON
-# Takes the file paths as arguments
-def make_json(csvFilePath, jsonFilePath):
-     
-    # create a dictionary
-    data = []
-     
-    # Open a csv reader called DictReader
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
-         
-        # Convert each row into a dictionary 
-        # and add it to data
-        for rows in csvReader:
-             
-            # Assuming a column named 'No' to
-            # be the primary key
-            #=key = rows['No']
-            #data[key] = rows
-            data = rows
- 
-    # Open a json writer, and use the json.dumps() 
-    # function to dump data
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(data, indent=4))
-         
-# Driver Code
- 
-
- 
-# Call the make_json function
-make_json(csvFilePath, jsonFilePath)
-'''
